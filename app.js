15a39c0d42b68b3916bc769e8126f6cae672bc7e
devOck <respecttowardyou@gmail.com>
2022-10-11 19:49:07 +0900

    ock 두번째

--- app.js
+++ app.js
@@ -106,7 +106,9 @@ process.on("unhandledRejection", (err, promise) => {
   server.close(() => process.exit(1));
 });
 
-console.log("TEST+")
+console.log("TEST+");
 // 주석처리 추가
 
 // 주석 처리 추가 2
+
+// 옥 추가
