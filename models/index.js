20b51b546b11f8fa7105283821803b8e78d13b7e
devOck <respecttowardyou@gmail.com>
2022-10-14 17:53:57 +0900

    14일-2

--- models/index.js
+++ models/index.js
@@ -4,7 +4,6 @@ const config = require("../config/config")[env];
 const User = require("./user");
 const Spaceship = require("./spaceship");
 const Scoredata = require("./scoredata");
-const Domain = require("./domain");
 
 const db = {};
 const sequelize = new Sequelize(
@@ -20,16 +19,13 @@ db.sequelize = sequelize;
 db.User = User;
 db.Spaceship = Spaceship;
 db.Scoredata = Scoredata;
-db.Domain = Domain;
 
 User.init(sequelize);
 Spaceship.init(sequelize);
 Scoredata.init(sequelize);
-Domain.init(sequelize);
 
 User.associate(db);
 Spaceship.associate(db);
 Scoredata.associate(db);
-Domain.associate(db);
 
 module.exports = db;
