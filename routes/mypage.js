171811ccdfb86361c83a7d7b8ca37265620dab74
devOck <respecttowardyou@gmail.com>
2022-10-16 21:05:31 +0900

    restful API 적용

--- routes/mypage.js
+++ routes/mypage.js
@@ -36,4 +36,77 @@ router.get("/", verifyToken, async (req, res, next) => {
   }
 });
 
+
+// 회원정보수정(user테이블 전체에서 nickname 중복되는 게 없다면,  password)
+router.put("/auth-update", verifyToken, async (req, res, next) => {
+  try {
+    console.log("PUT /auth/update 진입");
+    const { nick, password } = req.body;
+    const sameNick = await User.findOne({ where: { nick: nick } });
+    // 입력한 nick이나 password가 있다면
+    if (nick || password) {
+      // 입력한 nick이 있다면
+      if (nick) {
+        // 입력한 nick이 DB에 일치하는 것이 없다면
+        if (!sameNick) {
+          await User.update(
+            { nick: nick },
+            // { where: { id: req.headers.userid } }
+            { where: { id: req.decoded.id } }
+          );
+        }
+        // 입력한 nick이 DB에 일치하는 것이 있다면
+        else
+          return res.status(400).json({
+            message: "unavailable nickname",
+          });
+      }
+      if (password) {
+        const newPassword = await bcrypt.hash(password, 12);
+        User.update(
+          {
+            password: newPassword,
+          },
+          // { where: { id: req.headers.userid } }
+          { where: { id: req.decoded.id } }
+        );
+      }
+      const user = await User.findOne(
+        { where: { id: req.decoded.id } },
+        { attribues: ["nick"] }
+      );
+      return res.status(201).json({
+        message: "update-success",
+        data: user,
+      });
+    }
+    // 입력한 nick이나 password가 없다면
+    else
+      return res.status(400).json({
+        message: "no nick & no password",
+      });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: "update-failure",
+    });
+  }
+});
+
+// 회원탈퇴
+router.delete("/auth-delete", verifyToken, async (req, res, next) => {
+  try {
+    // await User.destroy({ where: { id: req.headers.userid } });
+    await User.destroy({ where: { id: req.decoded.id } });
+    return res.status(200).json({
+      message: "delete-success",
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: "delete-failure",
+    });
+  }
+});
+
 module.exports = router;
