3fc3d23a2627dc497ae0346ff9e8b3f55a3044a4
IvanInITworld <110373165+IvaninITworld@users.noreply.github.com>
2022-11-02 19:38:00 +0900

    prettier

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const {
   joinServiceEmailCheck,
   joinServiceNickCheck,
   joinService,
   loginService,
-} = require('../controllers/auth');
+} = require("../controllers/auth");
 
 const router = express.Router();
 
 // 회원가입;
 // 1. email 중복 검사
-router.post('/join/email-check', joinServiceEmailCheck);
+router.post("/join/email-check", joinServiceEmailCheck);
 // 2. nick 중복 검사
-router.post('/join/nick-check', joinServiceNickCheck);
+router.post("/join/nick-check", joinServiceNickCheck);
 // 3. 최종 검사(email, nick) 후, User 모델에 저장
-router.post('/join', joinService);
+router.post("/join", joinService);
 // 로그인
-router.post('/login', loginService);
+router.post("/login", loginService);
 
 module.exports = router;
