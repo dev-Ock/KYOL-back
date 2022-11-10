d9726650b03064913d713103ccf52ad2432c36d0
devOck <respecttowardyou@gmail.com>
2022-11-10 10:32:10 +0900

    loadtest

--- routes/game.js
+++ routes/game.js
@@ -5,7 +5,8 @@ const { gearPage, gameResultUpdate } = require('../controllers/game');
 
 const router = express.Router();
 
-router.get('/gear', verifyToken, apiLimiter, gearPage);
+router.get('/gear', verifyToken, gearPage);
+// router.get('/gear', verifyToken, apiLimiter, gearPage);
 router.put('/update', verifyToken, gameResultUpdate);
 
 module.exports = router;
