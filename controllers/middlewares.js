32adcb4ef5f749e37c4d6e582a0c380e6bf063aa
devOck <respecttowardyou@gmail.com>
2022-11-12 17:35:22 +0900

    verifyToken unitTest

--- controllers/middlewares.js
+++ controllers/middlewares.js
@@ -1,39 +1,42 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const RateLimit = require('express-rate-limit');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const RateLimit = require("express-rate-limit");
+const { resStatus } = require("../lib/responseStatus");
 
 const router = express.Router();
 
 // token 유효성 검사
 exports.verifyToken = (req, res, next) => {
   try {
-    console.log('middeleware 진입');
+    // console.log("middeleware 진입");
     req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-    console.log('req.decode : ', req.decoded);
+    // console.log("req.decode : ", req.decoded);
     return next();
     // return res.status(200).json({
     //   message: "verifyToken success",
     // });
   } catch (error) {
-    console.log('error : ', error);
+    console.log("error : ", error);
     // console.log("errorname : ", error.name);
-    if (error.name === 'TokenExpiredError') {
+    if (error.name === "TokenExpiredError") {
       // 유효기간 초과
-      return res.status(419).json({
-        code: 419,
-        message: '토큰이 만료되었습니다',
+      return res.status(resStatus.expired.code).json({
+        // 419
+        message: resStatus.expired.message,
+        // expired token
       });
     }
-    return res.status(401).json({
-      code: 401,
-      message: '유효하지 않은 토큰입니다',
+    return res.status(resStatus.invalidt.code).json({
+      // 401
+      message: resStatus.invalidt.message,
+      // invalidt
     });
   }
 };
 
 // game/gear 페이지 요청 횟수 제한(user type에 따라) (DoS 방지)
 exports.apiLimiter = router.use(async (req, res, next) => {
-  if (req.decoded.type === 'premium') {
+  if (req.decoded.type === "premium") {
     premiumApiLimiter(req, res, next);
   } else {
     freeApiLimiter(req, res, next);
@@ -46,7 +49,7 @@ const premiumApiLimiter = RateLimit({
   handler(req, res) {
     res.status(this.statusCode).json({
       code: this.statusCode,
-      message: 'premium limit',
+      message: "premium limit",
     });
   },
 });
@@ -57,7 +60,7 @@ const freeApiLimiter = RateLimit({
   handler(req, res) {
     res.status(this.statusCode).json({
       code: this.statusCode,
-      message: 'free limit',
+      message: "free limit",
     });
   },
 });
--- routes/game.js
+++ routes/game.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const { verifyToken } = require('./middlewares');
-const { apiLimiter } = require('./middlewares');
-const { gearPage, gameResultUpdate } = require('../controllers/game');
+const express = require("express");
+const { verifyToken } = require("../controllers/middlewares");
+const { apiLimiter } = require("../controllers/middlewares");
+const { gearPage, gameResultUpdate } = require("../controllers/game");
 
 const router = express.Router();
 
-router.get('/gear', verifyToken, gearPage);
+router.get("/gear", verifyToken, gearPage);
 // router.get('/gear', verifyToken, apiLimiter, gearPage);
-router.put('/update', verifyToken, gameResultUpdate);
+router.put("/update", verifyToken, gameResultUpdate);
 
 module.exports = router;
