fde56e0ffdebf55af1e2fd40ba30f507f7847a67
_devOck <respecttowardyou@gmail.com>
2022-11-23 13:06:15 +0900

    post, comment, postlike 테이블 추가 및 on delete cascade 적용

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
-const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
-// const { User, Spaceship, Scoredata, Shipdata } = require('../models');
-const User = require('./user');
-const Spaceship = require('./spaceship');
-const Scoredata = require('./scoredata');
-const Shipdata = require('./shipdata');
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
+const User = require("./user");
+const Spaceship = require("./spaceship");
+const Scoredata = require("./scoredata");
+const Shipdata = require("./shipdata");
+const Post = require("./post");
+const Comment = require("./comment");
+const Postlike = require("./postlike");
 
 const db = {};
 const sequelize = new Sequelize(
@@ -13,7 +15,7 @@ const sequelize = new Sequelize(
   config.username,
   config.password,
   config,
-  { timezone: '+09:00' }
+  { timezone: "+09:00" }
 );
 
 db.sequelize = sequelize;
@@ -21,15 +23,24 @@ db.User = User;
 db.Spaceship = Spaceship;
 db.Scoredata = Scoredata;
 db.Shipdata = Shipdata;
+db.Post = Post;
+db.Comment = Comment;
+db.Postlike = Postlike;
 
 User.init(sequelize);
 Spaceship.init(sequelize);
 Scoredata.init(sequelize);
 Shipdata.init(sequelize);
+Post.init(sequelize);
+Comment.init(sequelize);
+Postlike.init(sequelize);
 
 User.associate(db);
 Spaceship.associate(db);
 Scoredata.associate(db);
 Shipdata.associate(db);
+Post.associate(db);
+Comment.associate(db);
+Postlike.associate(db);
 
 module.exports = db;
